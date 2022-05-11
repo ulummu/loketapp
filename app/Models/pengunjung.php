7959835a27af7c8dde1f54da5e4179b41e4e0ef0
mh.ulum <misbahul0ulum@gmail.com>
2022-05-11 08:22:24 +0700

    menghilangkan has factory user

--- app/Models/pengunjung.php
+++ app/Models/pengunjung.php
@@ -2,12 +2,12 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+// use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Pengunjung extends Model
 {
-    use HasFactory;
+    // use HasFactory;
 
     protected $fillable = ['nik', 'nama'];
 
