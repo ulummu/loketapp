193b9c55c86b9abec40a95f69605e6a3b9a3aee2
mh.ulum <misbahul0ulum@gmail.com>
2022-05-14 23:42:02 +0700

    disable sabtu minggu

--- public/loket.js
+++ public/loket.js
@@ -183,9 +183,8 @@ $("#datepicker").datepicker({
   beforeShowDay: function (date) {
     var dayOfWeek = date.getDay();
     // 0 : Sunday, 1 : Monday, ...
-    // if (dayOfWeek == 0 || dayOfWeek == 6) return [false];
-    // else
-    return [true];
+    if (dayOfWeek == 0 || dayOfWeek == 6) return [false];
+    else return [true];
   },
 });
 
